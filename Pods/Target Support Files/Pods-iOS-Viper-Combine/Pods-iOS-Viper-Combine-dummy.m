9df8500144a6481b06b1dd52aa02ff0362f09eca
mculha <melih.culha.09@gmail.com>
2023-08-03 23:42:32 +0300

    Performs 'pod init' and Creates Some Folders

---
+++ Pods/Target Support Files/Pods-iOS-Viper-Combine/Pods-iOS-Viper-Combine-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_iOS_Viper_Combine : NSObject
+@end
+@implementation PodsDummy_Pods_iOS_Viper_Combine
+@end
---
+++ iOS-Viper-Combine/Utility/Constant.swift
@@ -0,0 +1,12 @@
+//
+//  Constant.swift
+//  iOS-Viper-Combine
+//
+//  Created by Melih Çulha on 3.08.2023.
+//
+
+import Foundation
+
+struct Constant {
+    static let API_KEY_NY = "4R5aDbTeDazPErTFmSkSmziQfdqNNfPZ"
+}
